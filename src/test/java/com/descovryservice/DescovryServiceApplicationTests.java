fcdfd76515ea52b4e2637cd2168a051923d70bc9
See Yassine <seeyassine@gmail.com>
2024-12-11 19:12:35 +0100

    first commit

---
+++ src/test/java/com/descovryservice/DescovryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.descovryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescovryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
